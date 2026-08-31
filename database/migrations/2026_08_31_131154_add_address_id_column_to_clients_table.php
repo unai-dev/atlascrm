dd41d5d3c0f6c2633938ffbcb767d643a50f1030
Unai Villar <unaidevcoding@gmail.com>
2026-08-31 15:13:43 +0200

    feat: add address_id column to clients table migration

---
+++ database/migrations/2026_08_31_131154_add_address_id_column_to_clients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->unsignedBigInteger("address_id");
+            $table->foreign("address_id")->references("id")->on("addresses")->onDelete("restrict");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropForeign("address_id");
+            $table->dropColumn("address_id");
+        });
+    }
+};
