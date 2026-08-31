6d75f1cb2d173b0bdf510a6ce3abf983096aa326
Unai Villar <unaidevcoding@gmail.com>
2026-08-31 15:13:26 +0200

    feat: create addresses table migration

---
+++ database/migrations/2026_08_31_130510_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string("main_address");
+            $table->string("second_address")->nullable(true);
+            $table->unsignedInteger("postal_code");
+            $table->string("country");
+            $table->string("autonomous_community");
+            $table->string("city");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
