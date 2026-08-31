b7ef087acf8924f8daeeb835008560eeff345ba6
Unai Villar <unaidevcoding@gmail.com>
2026-08-31 15:01:47 +0200

    feat: initial configuration

--- config/auth.php
+++ config/auth.php
@@ -16,7 +16,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        // 'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
