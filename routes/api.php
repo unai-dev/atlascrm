e960fdf06a6182d037849577e7564883f1b6fee5
Unai Villar <unaidevcoding@gmail.com>
2026-09-01 19:17:53 +0200

    feat: register clients and addresses routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", fn() => response()->json(["message" => "¡AtlasCRM is running!"]));
+
+Route::middleware("jwt.auth")->group(function () {
+    Route::apiResource("/clients", ClientController::class);
+    Route::apiResource("/addresses", AddressController::class);
+});
