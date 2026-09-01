a87a0d63be70ee29b4d922f6fb1cd0b97d5bd158
Unai Villar <unaidevcoding@gmail.com>
2026-09-01 17:24:42 +0200

    feat: development index, show and destroy endpoints in AddressController

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->query("per_page", 5);
+        $page = $request->query("page", 0);
+        $offset = $page * $perPage;
+
+        $addresses = Address::skip($offset)->take($perPage)->get();
+        return response()->json(["data" => $addresses]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Address $address)
+    {
+        return response()->json(["data" => $address]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Address $address)
+    {
+        $address->delete();
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
