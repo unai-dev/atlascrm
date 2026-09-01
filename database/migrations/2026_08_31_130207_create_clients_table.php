f81101415e260aebb63d69faa01cf3ebf9d9d058
Unai Villar <unaidevcoding@gmail.com>
2026-09-01 17:36:33 +0200

    refactor: refactor phone number data type

--- database/migrations/2026_08_31_130207_create_clients_table.php
+++ database/migrations/2026_08_31_130207_create_clients_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string("first_name", 55);
             $table->string("last_name", 55);
             $table->unsignedInteger("age");
-            $table->unsignedBigInteger("phone")->nullable(true);
+            $table->string("phone")->nullable(true);
             $table->string("email");
             $table->softDeletes();
             $table->timestamps();
