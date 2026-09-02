176824b39799a492c803a4cb9b04168164a92c43
Unai Villar <unaidevcoding@gmail.com>
2026-09-02 08:58:43 +0200

    feat: add GeneralFormRequest class

---
+++ app/Http/Requests/GeneralFormRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+class GeneralFormRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(["errors" => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
