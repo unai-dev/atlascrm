5acc48ace7a478c250c8f7b5cde0e4e82b70fc06
Unai Villar <unaidevcoding@gmail.com>
2026-09-02 20:28:33 +0200

    feat: auth controller development

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterUserRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::create([
+            "name" => $validated["name"],
+            "email" => $validated["email"],
+            "password" => bcrypt($validated["password"])
+        ]);
+
+        return response()->json(["data" => $user], Response::HTTP_CREATED);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $validated = $request->validated();
+
+        try {
+            if (!$token = JWTAuth::attempt($validated)) {
+                return response()->json(["error" => "Invalid Credentials"], Response::HTTP_UNAUTHORIZED);
+            }
+
+            return $this->respondWithToken($token);
+        } catch (JWTException $ex) {
+            return response()->json(["error" => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private function respondWithToken(string $token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
