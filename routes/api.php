f9c7c98af5c6d26802d8c538adb8e013de0ca52b
Unai Villar <unaidevcoding@gmail.com>
2026-09-02 20:28:47 +0200

    fix: add api guard in auth.php file

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CountryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", fn() => response()->json(["message" => "¡AtlasCRM is running!"]));
+
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                          AUTH ROUTES
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"])->name("login");
 
 Route::middleware("jwt.auth")->group(function () {
     Route::apiResource("/clients", ClientController::class);
