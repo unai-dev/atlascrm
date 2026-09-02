4a319d0c1d3b70288c72f1cecd27e2e37764ef30
Unai Villar <unaidevcoding@gmail.com>
2026-09-02 20:16:53 +0200

    feat: register cities and countries routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CountryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", fn() => response()->json(["message" => "¡AtlasCRM is running!"]));
@@ -9,4 +11,6 @@ Route::get("/", fn() => response()->json(["message" => "¡AtlasCRM is running!"]
 Route::middleware("jwt.auth")->group(function () {
     Route::apiResource("/clients", ClientController::class);
     Route::apiResource("/addresses", AddressController::class);
+    Route::apiResource("/cities", CityController::class);
+    Route::apiResource("/countries", CountryController::class);
 });
