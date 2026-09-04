bcdd287e7913c523213117770f885fac131ada61
Unai Villar <unaidevcoding@gmail.com>
2026-09-04 11:24:37 +0200

    feat: enterprise model creation

---
+++ app/Models/Enterprise.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Enterprise extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'observations',
+        'NIF',
+        'web_url',
+        'address_id'
+    ];
+
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function clients()
+    {
+        return $this->hasMany(Client::class);
+    }
+}
