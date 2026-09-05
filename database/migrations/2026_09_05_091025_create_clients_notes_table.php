aaaf58b5bd799616ca8d3822b631aff817a8c9c5
Unai Villar <unaidevcoding@gmail.com>
2026-09-05 11:27:33 +0200

    feat: create clients_notes table migration

---
+++ database/migrations/2026_09_05_091025_create_clients_notes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients_notes', function (Blueprint $table) {
+            $table->id();
+            $table->text("note");
+            $table->unsignedBigInteger("client_id");
+            $table->foreign("client_id")->references("id")->on("clients")->onDelete("cascade");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients_notes');
+    }
+};
